ce5ab62ffc5fbcbeb562a09029628fbf3fd747d6
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-02-28 01:41:50 +0530

    Create Odd_Even.cpp

---
+++ Odd_Even.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter the number" << endl;
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<< num << " is a even number";
+    }
+    else
+    {
+        cout<< num << " is a odd number";
+    }
+}
