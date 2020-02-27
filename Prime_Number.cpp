69e6607cde5d51c4f185b65bcf0dc62d8f2b92c1
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-02-28 02:02:49 +0530

    Create Prime_Number.cpp

---
+++ Prime_Number.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num,temp=0;
+    cout << "Enter the number" << endl;
+    cin >> num;
+    for(int i=2;i<num;i++)
+    {
+        if(num%i==0)
+        {
+            temp++;
+            brek;
+        }
+    }
+    if(temp==0)
+    {
+        cout << "Prime number";
+    }
+    else
+    {
+        cout << "Not a prime number";
+    }
+    return 0;
+}
