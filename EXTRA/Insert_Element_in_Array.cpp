b86eabb46a4a026dc13667df6de9144b16c3f6dc
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-03-02 23:20:25 +0530

    Update Insert_Element_in_Array.cpp

--- EXTRA/Insert_Element_in_Array.cpp
+++ EXTRA/Insert_Element_in_Array.cpp
@@ -8,7 +8,7 @@ int main()
     cout<<"Enter the no. of elements"<<endl;
     cin>>num;
     
-    int arr[num+1];
+    int arr[num+1];                                   // [num+1] -> Becoz we want to insert one more element in array i.e future prospective.
     
     cout<<"Enter the elements"<<endl;
     
@@ -22,13 +22,13 @@ int main()
 	cout<<"At which position (Enter index number) ? "<<endl;
 	cin>>pos;
     
-    for(int j=num+1;j>pos;j--)
+    for(int j=num+1;j>pos;j--)                       // Shift one value to insert the value in the selected position or index.
     {
         arr[j]=arr[j-1];
     }
-    arr[pos]=insert;
+    arr[pos]=insert;                                // Value inserted. 
     
-    for(int k=0;k<num+1;k++)
+    for(int k=0;k<num+1;k++)                        
     {
         cout<<arr[k]<<" ";
     }
