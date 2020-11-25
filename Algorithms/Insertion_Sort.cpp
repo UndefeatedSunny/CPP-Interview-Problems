43bea13e769647ba315af95af93a32186ea31faa
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-11-25 15:24:49 +0530

    Update Insertion_Sort.cpp

--- Algorithms/Insertion_Sort.cpp
+++ Algorithms/Insertion_Sort.cpp
@@ -1,35 +1,56 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long int 
 using namespace std;
 
-int main() {
-	int num,j;
-	int temp=0;
-
-	cin >> num;	
-	int arr[num];
-	
-	for(int in=0;in<num;in++)           
-	{
-	    cin>>arr[in];
-	}
-	
-	for(int i=1;i<num;i++)
-	{
-	    j=i-1;
-	    temp=arr[i];
-	    {
-	        while(j>=0 && arr[j]>temp)  
-	        {
-	            arr[j+1]=arr[j];
-	            j--;
-	        }
-	        arr[j+1]=temp;
-	    }
+void print(ll arr[] , ll num)           // Printing 
+{
+    for(int i=0;i<num;i++)
+    {
+        cout<<arr[i]<<" ";
     }
-	
-	for(int out=0;out<num;out++)
-	{
-	    cout<<arr[out]<<" ";	      
-	}  
-	return 0;
+    cout<<endl;
+}
+
+
+void insertion_sort(ll arr[] , ll num)     // Sorting 
+{
+    ll key;
+    ll j=0;
+    
+    for(ll i=1;i<num;i++)
+    {
+        key = arr[i];
+        j=i-1;
+        
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+
+
+int main()                               // Main
+{
+    ll num;
+    cin>>num;
+    
+    ll arr[num];
+    
+    for(int i=0;i<num;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    cout<<"BEFORE SORTING : ";
+    print(arr,num);
+    
+    insertion_sort(arr,num);
+    
+    cout<<"AFTER SORTING : ";
+    print(arr,num);
+    
+    return 0;
 }
